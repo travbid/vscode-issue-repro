1bdb59722c0282c0f1aca60300dabc3d426e72d1
Travers Biddle <travers.biddle@soulmachines.com>
2024-05-21 22:43:39 +1200

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// typedef __SIZE_TYPE__ size_t;
+// #define __building_module(x) 1
+
+// Uncommenting any of the 3 lines below makes red squiggles appear. Uncommenting either of the lines above resolves the issue.
+#include <chrono>
+// #include <iostream>
+// #include <print>
+
+#include <span>
+#include <unordered_map>
+#include <unordered_set>
+
+
+
+using std::span;
+using std::unordered_map;
+using std::unordered_set;
+
+int main(int argc, char **argv) {  //
+  return 0;
+}
